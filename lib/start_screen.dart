24c74096c211eb0e9ee554e2962b4a7bf4acb1fd
NV Rastogi <rastoginishant@yahoo.in>
2023-07-06 16:59:07 +0530

    Added Routing & Main Screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.onStartQuiz, {super.key});
+
+  final void Function() onStartQuiz;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(100, 255, 255, 255),
+          ),
+          // Opacity(
+          //   opacity: 0.1,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: onStartQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
