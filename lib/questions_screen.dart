e3811dc61153ec76c63ff89a73e366f59c6d998f
NV Rastogi <rastoginishant@yahoo.in>
2023-07-11 10:43:33 +0530

    Added Question & Answers(Styles too)

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,8 +12,42 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0; // initial question index
+
+  void onQuestionAnswered() {
+    setState(() {
+      currentQuestionIndex < 5
+          ? currentQuestionIndex++
+          : currentQuestionIndex = 0; // Incrementing the index
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: onQuestionAnswered,
+              );
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
